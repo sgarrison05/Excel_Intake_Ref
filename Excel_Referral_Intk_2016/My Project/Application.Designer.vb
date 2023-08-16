45a54c29a75194e7b580a136367161e7027613bc
Shon Garrison <smgarrison05@gmail.com>
2023-08-16 15:18:37 -0500

    Renamed Project. Added .vs to .gitignore.

--- Excel_Referral_Intk_2016/My Project/Application.Designer.vb
+++ Excel_Referral_Intk_2016/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Excel_Referral_Intk_2016.frmMain
+            Me.MainForm = Global.Excel_Intk_Ref_2023.frmMain
         End Sub
     End Class
 End Namespace
